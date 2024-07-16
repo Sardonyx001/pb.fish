53bf7ded8037035f3212f33c385aeb5a82544058
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2024-07-16 15:36:11 +0900

    feat: :memo: add example usage to help output

--- functions/pb.fish
+++ functions/pb.fish
@@ -1,7 +1,13 @@
 function pb --description "Uploads a file or data to a 0x0 paste bin service"
+
+    # Init variables
+    set -l VERSION "v2023.12.04"
+    set -l ENDPOINT "https://0x0.st"
+    set -l DATA ""
+    set -l EXT ""
     function show_help
         echo "pb [options] filename"
-        echo or
+        echo "or "
         echo "(command-with-stdout) | pb"
         echo ""
         echo "Uploads a file or data to the tilde 0x0 paste bin"
@@ -13,6 +19,16 @@ function pb --description "Uploads a file or data to a 0x0 paste bin service"
         echo "  -c | --color)                   Pretty color output"
         echo "  -s | --server server_address)   Use alternative pastebin server address"
         echo "  -e | --extension bin_extension) Specify file extension used in the upload"
+        echo ""
+        echo "Example usage:"
+        echo "  # Upload 'scores.txt' to the pastebin"
+        echo "  pb scores.txt"
+        echo "  # Upload piped output to the pastebin"
+        echo "  echo 'Secret info' | pb"
+        echo "  # Upload a list of javascript files to the pastebin individually"
+        echo "  find . -type f -name '*.js' -print | pb -f"
+        echo "  # Upload a file to a different pastebin endpoint"
+        echo "  pb -s http://0x0.st scores.txt"
     end
     echo pb
 end
